f9e37c7033613b28212615700ff3ac9155592603
Danylo Hermanovych <elluyema@gmail.com>
2024-06-17 17:05:42 +0300

    add integration test for mail send application

--- src/modules/exchange-rate/application/integration-test/fetch-exchange-rate.application.int-spec.ts
+++ src/modules/exchange-rate/application/integration-test/fetch-exchange-rate.application.int-spec.ts
@@ -1,10 +1,14 @@
-import { ScheduleModule } from '@nestjs/schedule';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppConfigModule } from 'src/shared/infrastructure/config/app-config.module';
+import { AppConfigServiceImpl } from 'src/shared/infrastructure/config/app-config.service';
+import { TYPES as SHARED_CONFIG_TYPES } from 'src/shared/infrastructure/ioc/types';
 
 import { ExchangeRate } from '../../domain/entities/exchange-rate.entity';
-import { ExchangeRateModule } from '../../exchange-rate.module';
+import { ExchangeRateServiceImpl } from '../../domain/services/exchange-rate.service';
+import { ExchangeRateClientImpl } from '../../infrastructure/http/clients/exchange-rate.client';
 import { TYPES } from '../../infrastructure/ioc';
 import { FetchExchangeRateApplicationImpl } from '../fetch-exchange-rate.application';
 
@@ -13,16 +17,37 @@ const fetchExchangeRateApp = {
   useClass: FetchExchangeRateApplicationImpl,
 };
 
+const exchangeRateService = {
+  provide: TYPES.services.ExchangeRateService,
+  useClass: ExchangeRateServiceImpl,
+};
+
+const exchangeRateClient = {
+  provide: TYPES.infrastructure.ExchangeRateClient,
+  useClass: ExchangeRateClientImpl,
+};
+
+const appConfigService = {
+  provide: SHARED_CONFIG_TYPES.infrastructure.AppConfigService,
+  useClass: AppConfigServiceImpl,
+};
+
 describe('FetchExchangeRateApplicationImpl integration', () => {
   let application: FetchExchangeRateApplicationImpl;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AppConfigModule, ScheduleModule.forRoot(), ExchangeRateModule],
+      imports: [AppConfigModule, ConfigModule, HttpModule],
+      providers: [
+        fetchExchangeRateApp,
+        exchangeRateClient,
+        exchangeRateService,
+        appConfigService,
+      ],
     }).compile();
 
     application = module.get<FetchExchangeRateApplicationImpl>(
-      fetchExchangeRateApp.provide,
+      TYPES.applications.FetchExchangeRateApplication,
     );
   });
 
