b32b1d4912e0aff19560e0d15eb533950d6be67f
Danylo Hermanovych <elluyema@gmail.com>
2024-07-03 22:54:55 +0300

    fix comments

--- src/modules/mailer/domain/factories/email.factory.ts
+++ src/modules/mailer/domain/factories/email.factory.ts
@@ -9,24 +9,6 @@ export class EmailFactory {
   }) {
     const { to, subject, text, html } = data;
 
-    this.validateEmailAddresses(to);
-
-    return new Email(to, subject, text, html);
-  }
-
-  private static validateEmailAddresses(emailAddresses: string[]): void {
-    const emailRegex =
-      /^((?:[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~]|(?<=^|\.)"|"(?=$|\.|@)|(?<=".*)[ .](?=.*")|(?<!\.)\.){1,64})(@)((?:[A-Za-z0-9.\-])*(?:[A-Za-z0-9])\.(?:[A-Za-z0-9]){2,})$/;
-    const invalidEmails = emailAddresses.filter(
-      (email) => !emailRegex.test(email),
-    );
-
-    if (!emailAddresses.length) {
-      throw new Error(`Recipient array can't be empty`);
-    }
-
-    if (invalidEmails.length > 0) {
-      throw new Error(`Invalid email addresses: ${invalidEmails.join(', ')}`);
-    }
+    return new Email({ to, subject, text, html });
   }
 }
