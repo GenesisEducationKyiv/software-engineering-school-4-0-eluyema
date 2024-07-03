6c006c26e3c1747428a071b2ff8858f3c80798e8
Danylo Hermanovych <elluyema@gmail.com>
2024-07-04 01:10:41 +0300

    make code more readable

--- arch-test/arch.spec.ts
+++ arch-test/arch.spec.ts
@@ -3,9 +3,11 @@ import 'tsarch/dist/jest';
 // imports the files entrypoint
 import { filesOfProject } from 'tsarch';
 
+const SECOND = 1000;
+
 describe('architecture', () => {
   // architecture tests can take a while to finish
-  jest.setTimeout(60000);
+  jest.setTimeout(60 * SECOND);
 
   // we use async await in combination with jest since this project uses asynchronous calls
   it('business logic should not depend on the ui', async () => {
